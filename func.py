7c020ac2898fc83b52c40101805fd379a82c6ae6
minhduc0711 <phamminhduc0711@gmail.com>
2020-03-06 11:02:15 +0700

    add force_rgb transform

--- func.py
+++ func.py
@@ -13,5 +13,12 @@ def normalize_images(x, mean=[0.485, 0.456, 0.406],
                      std=[0.229, 0.224, 0.225]):
     mean = torch.tensor(mean).to(x.device)
     std = torch.tensor(std).to(x.device)
-    
+
     return (x - mean.reshape(1, 3, 1, 1)) / std.reshape(1, 3, 1, 1)
+
+
+def force_rgb(img):
+    if img.mode != "RGB":
+        return img.convert("RGB")
+    else:
+        return img
